d02ad0e5c90288e58e15e8d98d51b3b8bd025fdb
ant <nivaniucc@gmail.com>
2021-05-23 17:02:56 -0400

    more functions

--- server.js
+++ server.js
@@ -41,21 +41,34 @@ function firstQuestion() {
         ],
       },
     ])
+    
     .then(function (data) {
       if (data.selection === "View All Employees") {
         viewEmployee();
       } else if (data.selection === "View All Employees By Manager") {
+        viewManager();
       } else if (data.selection === "View All Employees By Department") {
+        viewDepartment();
       } else if (data.selection === "Add Employee") {
+        addEmployee();
       } else if (data.selection === "Remove Employee") {
+        removeEmployee();
       } else if (data.selection === "Update Employee Role") {
+        updateRole();
       } else if (data.selection === "Update Employee Manager") {
+        updateManager();
       } else if (data.selection === "View All Roles") {
+        viewRoles();
       } else if (data.selection === "Add Role") {
+        addRole();
       } else if (data.selection === "Remove Role") {
+        removeRole();
       } else if (data.selection === "Add Department") {
+        addDepartment();
       } else if (data.selection === "View Department Budget") {
+        viewBudget();
       } else if (data.selection === "Remove Department") {
+        removeDepartment();
       } else data.selection === "Exit";
       connection.end();
     });
@@ -66,4 +79,48 @@ const viewEmployee = () => {
     console.table(res);
     firstQuestion();
   });
-}
+};
+
+const viewManager = () => {
+  connection.query(
+    "SELECT * FROM employees WHERE roles_id = 1", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    firstQuestion();
+  });
+};
+
+const viewDepartment = () => {
+  connection.query("SELECT * FROM departments", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    inquirer
+      .prompt([
+        {
+          name: "deptName",
+          type: "list",
+          message: "What department would you like to see?",
+          choices: [
+            "Engineering",
+            "Admin",
+            "Design",
+            "Labor",
+            "Back to Main Menu",
+          ],
+        },
+      ])
+      .then(function (data) {
+        if (data.deptName === "Engineering") {
+          connection.query(
+            "SELECT * FROM employees WHERE ? = department_id",
+            function (err, res) {
+              if (err) throw err;
+              console.table(res);
+              viewDepartment();
+            }
+          );
+        } else data.deptName === "Back to Main Menu";
+        firstQuestion();
+      });
+  });
+};
